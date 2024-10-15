47aaef99bb90f08b41be33f5526ac5cb90774330
tahreem-fatima17 <tahrim.fatymah@gmail.com>
2024-10-15 10:36:31 +0500

    Selenium course

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -5,6 +5,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import pages.HomePage;
 
 import java.time.Duration;
 import java.util.List;
@@ -12,7 +15,9 @@ import java.util.List;
 public class BaseTest
 {
     private WebDriver driver;
+    protected HomePage homePage;
 
+    @BeforeClass
     public void setUp()
     {
         System.setProperty("webdriver.chrome.driver", "resources\\chromedriver.exe");
@@ -20,23 +25,32 @@ public class BaseTest
 
         driver.get("https://the-internet.herokuapp.com/");
 
-        List<WebElement> links = driver.findElements(By.tagName("a"));
-        System.out.println(links.size());
-        System.out.println("yahan tk ho gya");
+        homePage = new HomePage(driver);
 
-        WebElement inputsLink = driver.findElement(By.linkText("Inputs"));
-        inputsLink.click();
+//        List<WebElement> links = driver.findElements(By.tagName("a"));
+//        System.out.println(links.size());
+//        System.out.println("yahan tk ho gya");
+//
+//        WebElement inputsLink = driver.findElement(By.linkText("Inputs"));
+//        inputsLink.click();
+//
+//        System.out.println(driver.getTitle());
+        System.out.println("");
 
-        System.out.println(driver.getTitle());
 
 
+    }
 
+    @AfterClass
+    public void teardown()
+    {
         driver.quit();
     }
 
-    public static void main(String[] args)
-    {
-        BaseTest test = new BaseTest();
-        test.setUp();
-    }
+
+//    public static void main(String[] args)
+//    {
+//        BaseTest test = new BaseTest();
+//        test.setUp();
+//    }
 }
